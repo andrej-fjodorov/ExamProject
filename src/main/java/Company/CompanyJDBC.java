f1b4cbf9f0ebecf55807238b3a873eb1441ef437
Andrey Fedorov <andrei.fedorov@inbox.ru>
2023-11-01 12:39:44 +0300

    Add CompanyJDBC class

---
+++ src/main/java/Company/CompanyJDBC.java
@@ -0,0 +1,42 @@
+package Company;
+import java.sql.ResultSet;
+import java.util.List;
+
+public class CompanyJDBC implements CompanyRepository{
+
+    private final static String GET_ALL_COMPANY="select*from company where \"deleted_at\" is null";
+    private final static String GET_ALL_ACTIVE_COMPANY="select*from company where \"is_active\"='true' and \"deleted_at\" is null";
+    private final static String ADD_NEW_COMPANY="insert into company (name, description) values (?,?)";
+    private final static String GET_LAST_ADDED_COMPANY="select*from company where \"deleted_at\" is null order by \"id\" desc limit 1";
+    private final static String DELETE_COMPANY="delete from company where \"id\"=?";
+
+    @Override
+    public List<Company> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Company> getAll(boolean isActive) {
+        return null;
+    }
+
+    @Override
+    public Company getById(int id) {
+        return null;
+    }
+
+    @Override
+    public int create(String name) {
+        return 0;
+    }
+
+    @Override
+    public int create(String name, String description) {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+}
